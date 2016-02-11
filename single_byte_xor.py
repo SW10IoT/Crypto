71a390d12adb747c230cf58645231093b5f06763
Thalmann <mr.thalmann@gmail.com>
2016-02-11 17:36:46 +0100

    File of hashes encrypted.

--- single_byte_xor.py
+++ single_byte_xor.py
@@ -5,14 +5,52 @@ jokes = b'ETAOIN SHRDLU'
 
 jokes_hex = binascii.hexlify(jokes)
 
+
+def load_file():
+    with open('encrypted.txt') as f:
+        hex_values = list()
+        for line in f:
+            hex_values.append(line.rstrip('\n'))
+    return hex_values
+
+def evaluate_decryption(decryptet_message):
+    score = 0
+    for c in decryptet_message:
+        x = ord(c)
+        if x == 32:
+            score += 10
+        elif (x > 32 and x < 65) or (x > 90 and x < 97) or (x > 122):
+            score += 1           
+        elif (x > 64 and x < 91) or (x > 96 and x < 123):
+            score += 10
+    return score
+
+
 def brute_force(some_input):
+    result = dict()
     splittet = [some_input[i:i+2] for i in range(0, len(some_input), 2)]
-    print(splittet)
     for x in range(0, 127):
         s = list()
         for v in splittet:
             s.append(chr(int(v,16) ^ x))
-        print(str(x) + ':' + chr(x) + '  :  ' + ''.join(s))
-        print('')
+        r = ''.join(s)
+        s = evaluate_decryption(r)
+        result[r] = s;
+        #print(str(x) + ':' + chr(x) + '  :  ' + r)
+        #print('Score: ' + str(s))
+        #print('')
+    return result
 
-brute_force(jokes_hex)
+def brute_force_file():
+    hex_values = load_file()
+    results = dict()
+    for h in hex_values:
+        results.update(brute_force(h))
+    return results
+
+import operator
+results = brute_force_file()
+results = sorted(results.items(), key=operator.itemgetter(1))
+for r in results:
+    print(r)
+
