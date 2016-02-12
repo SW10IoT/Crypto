9563085ca4f169ca741dc4875591ca02a6241a69
Thalmann <mr.thalmann@gmail.com>
2016-02-12 09:19:39 +0100

    PEP8'dified and imported module

--- break_repeating_key.py
+++ break_repeating_key.py
@@ -1,4 +1,5 @@
-import base64
+from base64 import b64decode
+import hamming_distance
 
 def load_file():
     r = ''
@@ -8,7 +9,7 @@ def load_file():
     return r
 
 def decode_base64(s):
-    return base64.b64decode(s)
+    return b64decode(s)
 
 def split_hex_string(hex_string):
     return [hex_string[i:i+2] for i in range(0, len(hex_string), 2)]
@@ -21,7 +22,11 @@ s = load_file()
 h = decode_base64(s)
 splittet_h =  split_hex_string(h)
 blocks = make_blocks(splittet_h, 10)
-print(blocks)
+
+
+
+#hamming distance test
+#print(hamming_distance.calculate('this is a test', 'wokka wokka!!!'))
 
 
 
--- hamming_distance.py
+++ hamming_distance.py
@@ -1,15 +1,13 @@
-def hamming(string1, string2):
+def calculate(string1, string2):
     r = 0;
     for x,y in zip(string1,string2):
-        r += bitLenCount(ord(x) ^ ord(y))
+        r += bit_len_count(ord(x) ^ ord(y))
     return r
 
 
-def bitLenCount(int_type):
+def bit_len_count(int_type):
     count = 0
     while (int_type):
         count += (int_type & 1)
         int_type >>= 1
     return(count)
-
-#print(hamming("this is a test","wokka wokka!!!"))
